d04120b92a77daa5f7be00ebd88c7eef0e7cd980
ragingwind <ragingwind@gmail.com>
2017-02-06 15:08:08 +0900

    step-1

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+process.env.NODE_ENV = 'development';
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: ['./src/index.js']
+  },
+  output: {
+    path: './build',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      include: path.resolve(__dirname, './src'),
+      loaders: 'babel-loader'
+    }]
+  },
+  plugins: [],
+  devServer: {
+    contentBase: './public',
+    inline: true,
+    host: 'localhost',
+    port: 8080
+  }
+};
