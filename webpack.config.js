b0f9b841060c72c9ba85794c3c10da95798ab493
ragingwind <ragingwind@gmail.com>
2017-02-10 11:08:21 +0900

    step-6, add build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
 'use strict';
 
-process.env.NODE_ENV = 'development';
-
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: ['./src/index.js']
-  },
-  output: {
-    path: './build',
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      include: path.resolve(__dirname, './src'),
-      loaders: 'babel-loader'
-    }]
-  },
-  plugins: [],
-  devServer: {
-    contentBase: './public',
-    inline: true,
-    host: 'localhost',
-    port: 8080
-  }
+module.exports = ({prod = false} = {}) => {
+
+  process.env.NODE_ENV = prod ? 'production' : 'development';
+
+  return {
+    entry: {
+      main: ['./src/index.js']
+    },
+    output: {
+      path: './build',
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [{
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, './src'),
+        loaders: 'babel-loader'
+      }]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+				inject: true,
+				template: './public/index.html',
+				favicon: './public/favicon.ico',
+				minify: {
+					removeComments: true,
+					collapseWhitespace: true,
+					removeRedundantAttributes: true,
+					useShortDoctype: true,
+					removeEmptyAttributes: true,
+					removeStyleLinkTypeAttributes: true,
+					keepClosingSlash: true
+				}
+			})
+    ],
+    devServer: {
+      contentBase: './public',
+      inline: true,
+      host: 'localhost',
+      port: 8080
+    }
+  };
 };
