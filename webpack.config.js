192dfacab084a776ce85c58dc716d6fa899cef79
ragingwind <ragingwind@gmail.com>
2017-02-10 17:30:15 +0900

    step-8, add servie-worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const devserver = process.argv[1].includes('webpack-dev-server');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const SWPrecacheWebpackPlugin = devserver ? require('sw-precache-webpack-dev-plugin') : require('sw-precache-webpack-plugin');
 
 module.exports = ({prod = false} = {}) => {
   process.env.NODE_ENV = prod ? 'production' : 'development';
@@ -11,8 +13,8 @@ module.exports = ({prod = false} = {}) => {
       main: ['./src/index.js']
     },
     output: {
-      path: './build',
-      filename: '[name].js'
+      path: path.resolve(__dirname, './build'),
+			filename: '[name].js'
     },
     module: {
       loaders: [{
@@ -24,8 +26,22 @@ module.exports = ({prod = false} = {}) => {
     plugins: [
       new CopyWebpackPlugin([{
         context: './public',
-        from: '*.+(png|json)'
+        from: '*.*'
       }]),
+      new SWPrecacheWebpackPlugin({
+        cacheId: require('./package.json').name,
+        stripPrefix: './build',
+        staticFileGlobs: [
+          path.join(path.resolve(__dirname, './build'), '**/*')
+        ],
+        maximumFileSizeToCacheInBytes: 4194304,
+        runtimeCaching: [{
+          handler: 'cacheFirst',
+          urlPattern: /https?:\/\/fonts.+/
+        }],
+        logger: function () {},
+        filename: 'sw.js'
+      })
     ],
     devServer: {
       contentBase: './public',
