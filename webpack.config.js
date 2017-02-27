34056e154e146431775917ceeb3d8aa9de64f028
ragingwind <ragingwind@gmail.com>
2017-02-27 23:29:40 +0900

    step-14-1, build for prod - minification js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,9 @@ const SWPrecacheWebpackPlugin = isWebpack ? require('sw-precache-webpack-plugin'
 const pkg = require('./package.json');
 const dotenvSafe = require('dotenv-safe').load();
 
-module.exports = () => {
+module.exports = ({production = false}) => {
+  process.env.NODE_ENV = production ? 'production' : 'development';
+
   const firebaseConfig = JSON.stringify({
     apiKey: process.env.FIREBASE_API_KEY,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
@@ -32,7 +34,8 @@ module.exports = () => {
     },
     plugins: [
       new DefinePlugin({
-        FIREBASE_CONFIG: firebaseConfig
+        FIREBASE_CONFIG: firebaseConfig,
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
       }),
       new CopyWebpackPlugin([{
         context: './public',
@@ -55,7 +58,8 @@ module.exports = () => {
           handler: 'networkFirst'
         }],
         logger: function () {},
-        filename: 'sw.js'
+        filename: 'sw.js',
+        minify: production
       }),
       new optimize.CommonsChunkPlugin({
         name: ['vendor']
@@ -68,6 +72,16 @@ module.exports = () => {
       port: 8080
     }
   };
+
+  if (production) {
+    webpackConfig.plugins = webpackConfig.plugins.concat([
+       new LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new optimize.UglifyJsPlugin()
+    ]);
+  }
 
   return webpackConfig;
 };
