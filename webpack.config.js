243ddf33def233effbad37fe0d2588529009b0dd
ragingwind <ragingwind@gmail.com>
2017-02-27 16:29:12 +0900

    Remove optimization options

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,12 @@
 const path = require('path');
 const {LoaderOptionsPlugin, DefinePlugin, optimize} = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SWPrecacheWebpackPlugin = require(`sw-precache-webpack-plugin`);
-const SWPrecacheWebpackDevPlugin = require(`sw-precache-webpack-dev-plugin`);
 const isWebpack = require('is-webpack');
+const SWPrecacheWebpackPlugin = isWebpack ? require('sw-precache-webpack-plugin') : require('sw-precache-webpack-plugin');
 const pkg = require('./package.json');
 const dotenvSafe = require('dotenv-safe').load();
 
-module.exports = ({production = false} = {}) => {
-  process.env.NODE_ENV = production ? 'production' : 'development';
-
+module.exports = () => {
   const firebaseConfig = JSON.stringify({
     apiKey: process.env.FIREBASE_API_KEY,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
@@ -35,7 +31,6 @@ module.exports = ({production = false} = {}) => {
     },
     plugins: [
       new DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         FIREBASE_CONFIG: firebaseConfig
       }),
       new CopyWebpackPlugin([{
@@ -48,23 +43,7 @@ module.exports = ({production = false} = {}) => {
           return content.toString().replace(/FIREBASE_CONFIG/, firebaseConfig);
         }
       }]),
-      new HtmlWebpackPlugin(Object.assign({
-        inject: true,
-        template: './public/index.html',
-        favicon: './public/favicon.ico', 
-      }, production ? {
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeStyleLinkTypeAttributes: true,
-          keepClosingSlash: true,
-          minifyJS: true
-        }
-      } : {})),
-      new (isWebpack ? SWPrecacheWebpackPlugin : SWPrecacheWebpackDevPlugin)({
+      new SWPrecacheWebpackPlugin({
         cacheId: `${pkg.name}-${pkg.version}`,
         stripPrefix: './build',
         staticFileGlobs: [
@@ -75,8 +54,7 @@ module.exports = ({production = false} = {}) => {
           handler: 'networkFirst'
         }],
         logger: function () {},
-        filename: 'sw.js',
-        minify: true
+        filename: 'sw.js'
       })
     ],
     devServer: {
@@ -86,16 +64,6 @@ module.exports = ({production = false} = {}) => {
       port: 8080
     }
   };
-
-  if (production) {
-    webpackConfig.plugins = webpackConfig.plugins.concat([
-       new LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      new optimize.UglifyJsPlugin()
-    ]);
-  }
 
   return webpackConfig;
 };
